20b6aee6908cf489856260c84e210741e5146f20
jm-parent <39386863+jm-parent@users.noreply.github.com>
2022-11-14 16:47:08 +0100

    Empty Project in MAUI APP

---
+++ BlazorApp-Empty/Program.cs
@@ -0,0 +1,11 @@
+using BlazorApp_Empty;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<EmptyApp>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
